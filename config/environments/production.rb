68a4e311d0649d9f874c26a6b471d40b95b4cf65
sivavishnu_k <sivavishnu.k@sigmainfo.net>
2021-04-21 12:05:29 +0530

    Login - Initial Commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,21 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => "localhost",
+      :user_name => ENV["SMTP_USER_EMAIL"],
+      :password => ENV["SMTP_USER_PASSWORD"],
+      :authentication => 'plain',
+      :enable_starttls_auto => true
+  }
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
