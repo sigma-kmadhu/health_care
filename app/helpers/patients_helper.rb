6de9518b506cbd53725097c686d3c7311c2bd67c
sivavishnu_k <sivavishnu.k@sigmainfo.net>
2021-05-19 15:52:10 +0530

    remove empty from array

--- app/helpers/patients_helper.rb
+++ app/helpers/patients_helper.rb
@@ -4,14 +4,18 @@ module PatientsHelper
         # Rails.cache.fetch('loc_services'){    
         #  }
         color_codes = ["#ff9999", "#9fdf9f", "#99c2ff", "#ffff99", "#dfbf9f", "#e6b3cc", "#80ffd4"]
-        locs = LocServiceDd.pluck(:loc).uniq
+        locs = reject_empty_from_array(LocServiceDd.pluck(:loc).uniq)
         service_hash = {}
         locs.each_with_index do |loc, index|
             service_hash[loc] = {
-                services: LocServiceDd.where(loc: loc).pluck(:service).uniq,
+                services: reject_empty_from_array(LocServiceDd.where(loc: loc).pluck(:service).uniq),
                 color: color_codes[index]
             }
         end
         service_hash
+    end
+
+    def reject_empty_from_array(array_obj)
+        array_obj.reject { |e| e.to_s.empty? }
     end
 end
