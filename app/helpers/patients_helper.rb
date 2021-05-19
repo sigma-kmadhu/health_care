4b26c7f93e89039c6b82514c39cee8530baca130
sivavishnu_k <sivavishnu.k@sigmainfo.net>
2021-05-19 15:43:54 +0530

    v: LOC changes and color coding

--- app/helpers/patients_helper.rb
+++ app/helpers/patients_helper.rb
@@ -1,15 +1,16 @@
 module PatientsHelper
-    # cache removed since the loc will be keep changing in DB by ELT team in postgres directly
+    # cache removed since the loc will be keep changing in DB by ETL team in postgres directly
     def construct_loc_services
         # Rails.cache.fetch('loc_services'){    
         #  }
+        color_codes = ["#ff9999", "#9fdf9f", "#99c2ff", "#ffff99", "#dfbf9f", "#e6b3cc", "#80ffd4"]
         locs = LocServiceDd.pluck(:loc).uniq
         service_hash = {}
-        locs.each do |loc|
-            priority_loc = LocServiceDd.where(loc: loc).pluck(:service) 
-            non_priority_loc = LocServiceDd.where.not(loc: loc).pluck(:service)
-            loc_service = priority_loc + non_priority_loc
-            service_hash[loc] = loc_service.flatten.uniq
+        locs.each_with_index do |loc, index|
+            service_hash[loc] = {
+                services: LocServiceDd.where(loc: loc).pluck(:service).uniq,
+                color: color_codes[index]
+            }
         end
         service_hash
     end
