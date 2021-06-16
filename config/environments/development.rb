8d8904316c366060f3a117d87097231db562cae7
Kavya Madhu <kavya.m@sigmainfo.net>
2021-06-16 15:48:57 +0530

    Changed mail host to smtp

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,8 +38,8 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
-  config.action_mailer.delivery_method = :letter_opener #:smtp
-
+  # config.action_mailer.delivery_method = :letter_opener #:smtp
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.smtp_settings = {
