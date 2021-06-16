8f948efeac251a53baa93bc5fbef87053c6802c7
Kavya Madhu <kavya.m@sigmainfo.net>
2021-06-16 18:02:14 +0530

    Committin production configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'http://3.218.27.182:3000/' }
 
   config.action_mailer.delivery_method = :smtp
 
