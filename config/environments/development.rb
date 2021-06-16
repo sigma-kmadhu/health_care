91f52db0d7062eee47560be0820fdcd0cb987e2c
Kavya Madhu <kavya.m@sigmainfo.net>
2021-06-16 16:42:10 +0530

    Changing the domain

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
       :address => "smtp.gmail.com",
       :port => 587,
-      :domain => "localhost",
+      :domain => "gmail.com",
       :user_name => ENV["SMTP_USER_EMAIL"],
       :password => ENV["SMTP_USER_PASSWORD"],
       :authentication => 'plain',
